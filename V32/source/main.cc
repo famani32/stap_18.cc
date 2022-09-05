9e12d6861e730b8c23421b2582d5b2acf6a855e4
famani32 <106656165+famani32@users.noreply.github.com>
2022-09-05 14:32:44 +0200

    Add files via upload

---
+++ V32/source/main.cc
@@ -0,0 +1,33 @@
+#include "apneaV7.h"
+
+int main(int argc, char *argv[]) {
+  try {
+    Utilities::MPI::MPI_InitFinalize mpi_initialization(argc, argv, 1);
+    dealii::deallog.depth_console(2);
+    apneaV9<3> elastic_problem_3d;
+    ParameterAcceptor :: initialize("lung.prm");
+    elastic_problem_3d.run(argv[1]);
+  } catch (std::exception &exc) {
+    std::cerr << std::endl
+              << std::endl
+              << "----------------------------------------------------"
+              << std::endl;
+    std::cerr << "Exception on processing: " << std::endl
+              << exc.what() << std::endl
+              << "Aborting!" << std::endl
+              << "----------------------------------------------------"
+              << std::endl;
+    return 1;
+  } catch (...) {
+    std::cerr << std::endl
+              << std::endl
+              << "----------------------------------------------------"
+              << std::endl;
+    std::cerr << "Unknown exception!" << std::endl
+              << "Aborting!" << std::endl
+              << "----------------------------------------------------"
+              << std::endl;
+    return 1;
+  }
+  return 0;
+}
